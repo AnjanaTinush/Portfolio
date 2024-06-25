6714ce5b55ce0a0c60a56bfdf98b4ceb3b3167b2
AnjanaTinush <146495563+AnjanaTinush@users.noreply.github.com>
2024-06-25 11:54:12 +0530

    create home page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { services } from '../components'
+import { fadeIn, textVariant } from '../utils' // Ensure this path is correct
 
 const About = () => {
   return (
