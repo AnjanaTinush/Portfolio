64f9bef073651900e6a40c764f2544f0834718a5
AnjanaTinush <146495563+AnjanaTinush@users.noreply.github.com>
2024-06-27 08:18:51 +0530

    completecontact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,33 +1,84 @@
-import { useState,useRef } from "react"
-import { motion } from "framer-motion"
-import emailjs from '@emailjs/browser'
-import {styles} from '../styles'
-import {EarthCanvas} from './canvas'
-import {SectionWrapper} from '../hoc'
-import { slideIn } from "../utils/motion"
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
   const formRef = useRef();
-  const [form,setForm]=useState({
-    name :'',
-    email:'',
-    message:'',
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
   });
-const [loaging,setLoading]=useState(false);
 
-const handleChange = (e) =>{}
+  const [loading, setLoading] = useState(false);
 
-const handleSubmit =(e) => {}
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    //ufmtp9PikT3PLPLYz
+    //template_q41is4k
+
+    //service_pajwpob
+    emailjs
+      .send(
+        "service_obafwl7",
+        "template_5vsp91i",
+        {
+          from_name: form.name,
+          to_name: "Anjana Tinush",
+          from_email: form.email,
+          to_email: "anjanatinush222@gmail.com",
+          message: form.message,
+        },
+        "mncx4XFocLsGh5bVP"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you...! I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert("Something went wrong. Please try again...!");
+        }
+      );
+  };
 
   return (
-    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+    <div
+      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+    >
       <motion.div
-      variants={slideIn('left','tween',0.2,1)}
-      className="flex-[0.75] bg-black-100 p-8 roundded-2xl"
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
       >
-   <p className={styles.sectionSubText}>Get in touch</p>
-   <h3 className={styles.sectionHeadText}>Contact.</h3>
-   <form
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+        <form
           ref={formRef}
           onSubmit={handleSubmit}
           className="mt-12 flex flex-col gap-8"
@@ -65,22 +116,24 @@ const handleSubmit =(e) => {}
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
-         <button
-         type="submit"
-         className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold sgadow-md sgadow-promary rounded-xl"
-         >
-          {loaging ? 'Sending...' : 'Send'}
-         </button>
-   
-   </form>
+
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
+          >
+            {loading ? "Sending..." : "Send"}
+          </button>
+        </form>
       </motion.div>
+
       <motion.div
-      variants={slideIn('right','tween',0.2,1)}
-      className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
-   <EarthCanvas/>
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
       </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default SectionWrapper(Contact,"contact")
+export default SectionWrapper(Contact, "contact");
