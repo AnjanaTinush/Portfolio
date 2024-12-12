8736e9ab0080af69287d237faba4c05153b96e97
AnjanaTinush <146495563+AnjanaTinush@users.noreply.github.com>
2024-12-12 11:31:26 +0530

    some changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#f03e3e]">Anjana Tinushhh</span>
+            Hi, I'm <span className="text-[#f03e3e]">Anjana Tinush</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Full Stack Developer...
